6c26d3dae5385f440f132f541296f2d171d06ecb
lingguangjun <64408475+lingguangjun@users.noreply.github.com>
2021-10-01 11:49:35 +0800

    pull_test

--- src/main/java/com/example/demo/test/GIt_test_01.java
+++ src/main/java/com/example/demo/test/GIt_test_01.java
@@ -3,5 +3,7 @@ package com.example.demo.test;
 public class GIt_test_01 {
     public static void main(String[] args) {
         System.out.println("01_test");
+        System.out.println("pull_test");
+        System.out.println("=====================================");
     }
 }
